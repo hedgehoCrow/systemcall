7f862cd9a28db98ca3b4aed62c02279fd157417a
Yuta Muraki <crowizard@gmail.com>
2017-07-12 20:01:19 +0900

    初期化スクリプト

---
+++ get_script.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/hedgehoCrow/systemcall/master/printk.c
+wget https://raw.githubusercontent.com/hedgehoCrow/systemcall/master/syscall_32.tbl
+wget https://raw.githubusercontent.com/hedgehoCrow/systemcall/master/syscalls.h
+wget https://raw.githubusercontent.com/hedgehoCrow/systemcall/master/test_program.c
+wget https://raw.githubusercontent.com/hedgehoCrow/systemcall/master/prog1.c
+wget https://raw.githubusercontent.com/hedgehoCrow/systemcall/master/copy.sh
+wget https://raw.githubusercontent.com/hedgehoCrow/systemcall/master/set.sh
+
+sh set.sh
---
+++ set.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mv syscall_32.tbl linux_3.3.9/arch/x86/syscalls/
+mv syscalls.h linux_3.3.9/include/linux/
+mv printk.c linux_3.3.9/kernel/
